1c7c2c489ddd3b8bb7f27da3f25b978e1f51a432
WietseTDX <WJTD1234@gmail.com>
2024-06-13 12:38:26 +0200

    Finished anedoctes exercise

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,34 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const SimpleButton = ({ onClick, children }) => {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  )
+}
+
+const getRandomInt = (min, max, prev = -1) => {
+  let random;
+  do {
+    random = Math.floor(Math.random() * (max - min)) + min;
+  } while (random === prev);
+  return random;
+};
+
+const getMaxIndex = (arr) => {
+  return arr.reduce((maxIndex, currentValue, currentIndex, array) => {
+    return currentValue > array[maxIndex] ? currentIndex : maxIndex;
+  }, 0);
+};
+
+const hasData = (arr) => {
+  return arr.reduce((hasData, value) => {
+    return hasData || value > 0
+  }, 0)
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(anecdotes.map(() => 0))
+  
+  const setDisplayedAnecdote = () => setSelected(getRandomInt(0, anecdotes.length, selected))
+
+  const voteForCurrentAnecdote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  }
 
   return (
-    <>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div style={{ height: '50px' }}>
+        <div>Anecdote: {anecdotes[selected]}</div>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>Vote: {votes[selected]}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <SimpleButton onClick={voteForCurrentAnecdote}>
+          Vote
+        </SimpleButton>
+        <SimpleButton onClick={setDisplayedAnecdote}>
+          Next anecdote
+        </SimpleButton>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div>
+        <h1>Anecdote with most votes</h1>
+      </div>
+      <div>
+        {hasData(votes) ? (
+          <div>
+            {anecdotes[getMaxIndex(votes)]} <br />
+            Has {votes[getMaxIndex(votes)]} votes!
+          </div>
+        ) : (
+          <b>Zero votes have been cast</b>
+        )}
+
+      </div>
+    </div>
   )
 }
 
--- anecdotes/src/main.jsx
+++ anecdotes/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App'
+
+const rootElement = document.getElementById('root')
+
+const root = ReactDOM.createRoot(rootElement)
+
+
+const refresh = () => {
+  root.render(
+    <div>
+      <App />
+    </div>
+  )
+}
+
+refresh()
